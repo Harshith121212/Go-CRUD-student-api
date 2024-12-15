b783734939524af4485d91d9b1dbe07af397fbc7
Harshith <harshithprao889@gmail.com>
2024-12-15 09:28:08 +0530

    Request Validation

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Harshith121212/crud-project/internal/types"
 	"github.com/Harshith121212/crud-project/internal/utils/response"
+	"github.com/go-playground/validator/v10"
 )
 
 func New() http.HandlerFunc {
@@ -21,6 +22,19 @@ func New() http.HandlerFunc {
 
 		if errors.Is(err, io.EOF) {
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("request body is empty")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		//validation of request
+
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
 			return
 		}
 
